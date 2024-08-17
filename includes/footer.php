5159d7818a6176ae17845eee3f234ade47328178
aniket156 <82699757+aniket156@users.noreply.github.com>
2024-08-17 12:30:43 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+       <footer>
+		<div class="container">
+			<center>
+			    <p>“Copyright © Lifestyle Store. All Rights Reserved | Contact Us: +91 90000 00000”.</p>
+			</center>
+		</div>
+	</footer>
+
+</body>
+</html>
